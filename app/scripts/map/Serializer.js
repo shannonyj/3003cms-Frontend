917b9c1f3567da1236a05f983353b47dadd7e921
snowblue <yhchankenny@gmail.com>
2016-11-05 01:30:36 +0800

    Pushed

--- app/scripts/map/Serializer.js
+++ app/scripts/map/Serializer.js
@@ -15,9 +15,23 @@ var Serializer = (function () {
             serializableArea['name'] = areaList[i].name;
             serializableArea['description'] = areaList[i].description;
             serializableArea['time'] = areaList[i].time;
+            serializableArea['location'] = areaList[i].location;
+            serializableArea['severity'] = areaList[i].severity;
             serializableAreaList.push(serializableArea);
         }
         return JSON.stringify(serializableAreaList);
+    };
+    Serializer.prototype.serializeArea = function (area) {
+        var serializableArea = {};
+        serializableArea['coordinates'] = area.serializablePolygon;
+        serializableArea['disaster'] = DisasterType[area.disaster];
+        serializableArea['type'] = AreaType[area.type];
+        serializableArea['name'] = area.name;
+        serializableArea['description'] = area.description;
+        serializableArea['time'] = area.time;
+        serializableArea['location'] = area.location;
+        serializableArea['severity'] = area.severity;
+        return JSON.stringify(serializableArea);
     };
     Serializer.prototype.deserializeAreaList = function (raw) {
         var deserialized = JSON.parse(raw);

a714eea99cc66eed1e27064699750286f0704ef3
shannon_z <zyjun01@163.com>
2016-11-07 18:49:37 +0800

    test button

--- app/scripts/controllers/js/SocialMedia.js
+++ app/scripts/controllers/js/SocialMedia.js
@@ -6,6 +6,21 @@
 (function() {
     'use strict';
     angular.module('myApp').controller('timelineCtrl', function ($scope,$mdDialog, social,CONSTANTS) {
+        $scope.showAlert = function(ev) {
+            // Appending dialog to document.body to cover sidenav in docs app
+            // Modal dialogs should fully cover application
+            // to prevent interaction outside of dialog
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .parent(angular.element(document.querySelector('#popupContainer')))
+                    .clickOutsideToClose(true)
+                    .title('This is an alert title')
+                    .textContent('You can specify some description text in here.')
+                    .ariaLabel('Alert Dialog Demo')
+                    .ok('Got it!')
+                    .targetEvent(ev)
+            );
+        };
         $scope.showAdvancedFb = function(ev, id) {
             console.log('facebook');
 
